1956bff729e650f4fa4466c25df61f8f55819380
lv-conner <lvmh123456@gmail.com>
2020-02-25 12:40:15 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { lazy, Suspense } from 'react';
+import { HashRouter,BrowserRouter,Route, Switch ,Link, Router } from 'react-router-dom';
+import { Menu, Icon } from 'antd';
+import "./App.css";
+
+const Home = lazy(() => import('./components/Home'));
+const List = lazy(() => import('./components/List'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            key:null
+        }
+    }
+    handleClick = ({ item, key, keyPath, domEvent }) => {
+        this.setState({
+            key:key
+        });
+        console.log(key)
+    }
+    renderComponents = () => {
+        var component = null;
+        switch (this.state.key) {
+            case "home":
+                component = <Home />
+                break;
+            case "list":
+                component = <List />
+                break;
+            default:
+                break;
+        }
+        return component;
+    }
+    componentDidMount(){
+        console.log(this.props);
+    }
+    render() {
+        return(
+            <div>
+                <div className="deader"></div>
+                <div className="content">
+                    <BrowserRouter>
+                        <div className="left">
+                            <Menu onClick={this.handleClick} style={{ width: 256 }} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline">
+                                <Menu.Item key="home"><Link to="/home">Home</Link></Menu.Item>
+                                <Menu.Item key="list"><Link to="/list">List</Link></Menu.Item>
+                            </Menu>
+                        </div>
+                        <div className="right">
+                            {/* <Suspense fallback={<Loading />}>
+                                {this.renderComponents()}
+                            </Suspense> */}
+                            <Suspense fallback={<Loading />}>
+                                <Switch>
+                                    <Route path="/home" exact component={props => <Home {...props} />} />
+                                    <Route path="/list" exact component={props => <List {...props} />} />
+                                </Switch>
+                            </Suspense>
+                        </div>
+                    </BrowserRouter>
+                </div>
+            </div>
+        )
+    }
+}
+
+function Loading() {
+    return <div>Loading...</div>
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component{
+    render(){
+        return <div>
+            Here is Home!
+        </div>
+    }
+}
+
+
+export default Home;
